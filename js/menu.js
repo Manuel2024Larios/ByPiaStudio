c1b9bbecc7c89047170063b805dacf2cc9a5c65a
Manuel2024Larios <lr0429012024@unab.edu.sv>
2024-12-22 04:54:39 -0600

    Add files via upload

---
+++ js/menu.js
@@ -0,0 +1,4 @@
+document.getElementById('menu-icon').addEventListener('click', function(){
+    const menuItems = document.getElementById('menu-options');
+    menuItems.classList.toggle('mostrar');
+});
